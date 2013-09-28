a6c78e041421603330ff20c61cf6fd6910143a22
lucaboieru <luca.boieru@yahoo.com>
2013-09-28 22:02:04 +0200

    Server update

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var net = require('net');
 
+var users = [];
+
 var server = net.createServer(function (socket) {
 	
-	socket.write('test');
+	users.push(socket);
+
+	socket.on('data', function (data) {
+		for(var user in users) {
+			if(users[user] === socket) continue;
+			users[user].write(data);
+		}
+	});
 
 });
 
